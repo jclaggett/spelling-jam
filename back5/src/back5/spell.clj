02c73f4b5d7954b576af0dff9a1945a3038a4d18
Chouser <chouser@lonocloud.com>
2013-07-08 15:53:16 -0400

    Fix get-words typo

--- back5/src/back5/spell.clj
+++ back5/src/back5/spell.clj
@@ -2,7 +2,7 @@
 
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 
-defn get-words []
+(defn get-words []
   (let [f (clojure.java.io/reader "resources/big.txt")
         lines-lower (map #(.toLowerCase %) (line-seq f))]
     (mapcat #(re-seq #"[a-z]+" %) lines-lower)))
